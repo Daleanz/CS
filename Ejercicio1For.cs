ddcd456b51370447428ec9effb6ceeb343691a08
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-09-08 22:33:58 -0300

    Create Ejercicio1For.cs
    Mostrar números pares entre [ 1 - 100 ] con ciclo For.

---
+++ Ejercicio1For.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Proyect1;
+
+public class Proyect{
+	public static void Main(string[] args){
+		// Mostrar numeros pares entre el [1 - 100]
+
+		for(int i = 1; i <= 100; i ++){
+			System.Console.WriteLine(i);
+		}
+
+
+	}
+}
