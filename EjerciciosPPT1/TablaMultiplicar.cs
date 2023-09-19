9d4527eb660ec50cb740d84b7c1c9eaa69f01baf
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-09-19 14:31:48 -0300

    TablaMultiplicar.cs
    Crear tabla de multiplicar que pida:
    
    - El número del cual se quiere ver la tabla.
    - El rango de la tabla.

---
+++ EjerciciosPPT1/TablaMultiplicar.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace TablaMultiplicar{
+    public class Program{
+        public static void Main(string[] args){
+
+            string mensaje = "Tabla de Multiplicar \r\n¿Tabla de que numero quiere ver?";
+            bool flag = true;
+
+            while(flag){
+                System.Console.WriteLine(mensaje);
+                System.Console.Write("Ingrese el numero: ");
+                int numero = System.Convert.ToInt32(System.Console.ReadLine());
+                System.Console.Write("Ingrese el rango de la tabla: ");
+                int rango = System.Convert.ToInt32(System.Console.ReadLine());
+
+                for(int i = 1; i <= rango; i ++){
+                    System.Console.WriteLine($"{numero} x {i} = {numero * i}");
+                }
+
+                System.Console.Write("¿Continuar? (1) Si: ");
+                int opcion = System.Convert.ToInt32(System.Console.ReadLine());
+                if(opcion == 1){
+                    flag = true;
+                }else{
+                    System.Console.WriteLine("Cerrando programa...");
+                    flag = false;
+                }
+            }
+        }
+    }
+}
