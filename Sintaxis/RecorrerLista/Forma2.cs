f2cc718b64057069c908c096216f91f395fbbf75
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-09-29 18:21:54 -0300

    Recorrer Lista con Do While
    Se hace uso del ciclo do while, para poder mostrar los elementos de una Lista.

---
+++ Sintaxis/RecorrerLista/Forma2.cs
@@ -0,0 +1,27 @@
+// iTz_DiLAN
+
+using System;
+
+namespace Prueba{
+	public class Program{
+		public static void Main(string[] args){
+			List<dynamic> Lista = new List<dynamic>();
+
+			Lista.Add("Messi");
+			Lista.Add("Ronaldo");
+
+			Lista.Add(7);
+			Lista.Add(5);
+
+			Lista.Add(1.34);
+			Lista.Add(1.20);
+
+			int aux = 0;
+
+			do{
+				System.Console.WriteLine(Lista[aux]);
+				aux ++;
+			}while(aux < Lista.Count() - 1);
+		}
+	}
+}
