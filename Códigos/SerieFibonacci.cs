cb7c987e32775be2da5e8bdc76a4190637d4f6ba
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-09-29 16:16:13 -0300

    Se agrego codigo de Serie de Fibonacci
    Código que calcula la Serie de Fibonacci de un numero ingresado por teclado.

---
+++ Códigos/SerieFibonacci.cs
@@ -0,0 +1,20 @@
+// iTz_DiLAN
+
+using System;
+
+namespace SerieFibonacci{
+    public class Program{
+        public static void Main(string[] args){
+            System.Console.Write("Ingrese el numero para la serie de fibonacci: ");
+            int num = System.Convert.ToInt32(System.Console.ReadLine());
+
+            int a = 0, b = 1;
+            for(int i = 0; i <= num; i ++){
+                System.Console.Write(a + " ");
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+        }
+    }
+}
