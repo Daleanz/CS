e0275df9ca23fdcd265e3c20255f8c9cf43d8ebf
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-09-29 15:37:41 -0300

    Se modifico el archivo

--- PruebaLabPrograS1/PruebaS2/Problema1.cs
+++ PruebaLabPrograS1/PruebaS2/Problema1.cs
@@ -1,70 +1,53 @@
 /*
 iTz_DiLAN
 
-Problema 3: Implementa un programa que permita al usuario ingresar un número entero positivo y luego calcule la suma de todos los números primos en el
-rango desde 2 hasta el número ingresado.
+Problema 1: Escribe un programa que permita al usuario ingresar un número entero positivo y luego calcule la cantidad de números pares e impares
+en el rango desde [ 1 ] hasta el número ingresado.
 
-El programa debe mostrar por pantalla la suma de los números primos.
+El programa debe mostrar por pantalla la cantidad de números pares e impares encontrados.
 */
 
 using System;
 
-namespace P3{
+namespace P1{
 	public class Program{
 		public static void Main(string[] args){
 			bool flag = true;
+
+			int cantidad_pares = 0, cantidad_impares = 0;
+
 			while(flag){
-				System.Console.Write("Ingrese el numero limite final del rango: ");
+				System.Console.Write("Ingrese un número entero positivo: ");
 				int numero_ingresado = System.Convert.ToInt32(System.Console.ReadLine());
 
 				if(numero_ingresado < 0){
-					System.Console.WriteLine("El numero ingresado es negativo, se transformo a positivo (+).");
+					System.Console.WriteLine("El numero es negativo, se cambiara a positivo.");
 					numero_ingresado = numero_ingresado * (-1);
 				}
-
-				if(numero_ingresado < 2){
-					System.Console.WriteLine("El numero tiene que ser mayor o igual que 2.");
+				if(numero_ingresado > 0){
+					for(int i = 1; i <= numero_ingresado; i ++){
+						if(i % 2 == 0){
+							cantidad_pares = cantidad_pares + 1;
+						}else{
+							cantidad_impares = cantidad_impares + 1;
+						}
+					}
 				}
+				System.Console.WriteLine($"Cantidad de números pares: {cantidad_pares}");
+				System.Console.WriteLine($"Cantidad de números impares: {cantidad_impares}");
 
-				int suma_primos = SumarPrimos(numero_ingresado);
-
-				System.Console.WriteLine($"La suma de los números primos en el rango de [ 2 a {numero_ingresado}] es: {suma_primos}");
-
-				System.Console.Write("¿Seguir? (1) Si:  ");
+				System.Console.Write("¿Seguir? (1) Si: ");
 				int opcion = System.Convert.ToInt32(System.Console.ReadLine());
 
 				if(opcion == 1){
-					suma_primos = 0;
+					cantidad_pares = 0;
+					cantidad_impares = 0;
 					flag = true;
 				}else{
 					System.Console.WriteLine("Cerrando programa...");
 					flag = false;
 				}
-
 			}
-		}
-
-		static bool VerificarPrimo(int numero){
-			if(numero < 2){
-				return false;
-			}
-			for(int i = 2; i <= Math.Sqrt(numero); i ++){
-				if(numero % i == 0){
-					return false;
-				}
-			}
-			return true;
-		}
-
-		static int SumarPrimos(int numero_limite){
-			int suma = 0;
-
-			for(int i = 2; i <= numero_limite; i ++){
-				if(VerificarPrimo(i)){
-					suma = suma + i;
-				}
-			}
-			return suma;
 		}
 	}
 }
