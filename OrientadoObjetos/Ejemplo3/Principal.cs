86c513d4366495d1ea6ed0d6d34e39532704f1bd
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-10-07 19:56:12 -0300

    Create Principal.cs
    Clase Principal del Ejemplo3

---
+++ OrientadoObjetos/Ejemplo3/Principal.cs
@@ -0,0 +1,19 @@
+// iTz_DiLAN
+
+using System;
+using Ejemplo3;
+
+namespace Ejemplo3{
+    public class Principal{
+	public static void Main(string[] args){
+	    NumerosNaturales n1 = new NumerosNaturales(10);
+
+	    n1.Suma(20);
+	    n1.EsPar();
+	    n1.EsPrimo();
+	    n1.Resta(5);
+	    n1.Multiplicacion(3);
+	    
+	}
+    }
+}
