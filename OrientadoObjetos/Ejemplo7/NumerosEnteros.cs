25d6314ec75bd5491eaff1216b8cc3a4efd4e050
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-10-15 23:20:28 -0300

    Clase NumerosEnteros
    Clase Numeros Enteros del Ejemplo 7

---
+++ OrientadoObjetos/Ejemplo7/NumerosEnteros.cs
@@ -0,0 +1,21 @@
+// iTz_DiLAN
+
+using System;
+
+namespace Ejemplo7{
+    public class NumerosEnteros{
+	private int valor;
+
+	public NumerosEnteros(int valor){
+	    this.valor = valor;
+	}
+
+	public void setValor(int valor){
+	    this.valor = valor;
+	}
+
+	public int getValor(){
+	    return this.valor;
+	}
+    }
+}
