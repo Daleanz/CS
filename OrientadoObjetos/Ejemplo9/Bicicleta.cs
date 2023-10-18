41806e932dfd4c5744d409aa832b44e5de4e5af1
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-10-18 18:34:43 -0300

    Create Bicicleta.cs
    Clase Bicicleta del Ejemplo 9

---
+++ OrientadoObjetos/Ejemplo9/Bicicleta.cs
@@ -0,0 +1,25 @@
+using System;
+using Vehiculos;
+
+namespace Vehiculos{
+  public class Bicicleta : Vehiculo{
+    private int velocidadesBici;
+
+    public Bicicleta(string marcaVehiculo, string modeloVehiculo, int anioVehiculo, int velocidadesBici) : base(marcaVehiculo, modeloVehiculo, anioVehiculo){
+      this.velocidadesBici = velocidadesBici;
+    }
+    
+    public int VelocidadesBici{
+      get{
+        return this.velocidadesBici;
+      }
+      set{
+        this.velocidadesBici = value;
+      }
+    }
+
+    public override void Describir(){
+      System.Console.WriteLine($"Informacion de la Bicicleta:\nModelo: {base.MarcaVehiculo}\nModelo: {base.ModeloVehiculo}\nAnio: {base.AnioVehiculo}\nVelocidades: {velocidadesBici}")
+    }
+  }
+}
