203d5f2d153b4c97c5e92ad4b1899d4b31c26c1c
iTz_DiLAN <87887352+Daleanz@users.noreply.github.com>
2023-10-24 11:46:57 -0300

    Create MantenimientoActivo.cs

---
+++ ActivosTecnologicos/MantenimientoActivo.cs
@@ -0,0 +1,42 @@
+using System;
+using ActivosTecnologicos;
+
+namespace FlotaVehiculos
+{
+    class MantenimientoActivo
+    {
+        private DateTime fecha;
+        private string descripcion;
+        private double costo;
+
+        public MantenimientoVehiculo(DateTime fecha, string descripcion, double costo)
+        {
+            this.fecha = fecha;
+            this.descripcion = descripcion;
+            this.costo = costo;
+        }
+
+        public DateTime Fecha
+        {
+            get { return fecha; }
+            set { this.fecha = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { this.descripcion = value; }
+        }
+
+        public double Costo
+        {
+            get { return costo; }
+            set { this.costo = value; }
+        }
+
+        public void mostrarInformacion()
+        {
+            System.Console.WriteLine($"Informacion del Mantenimiento:\nFecha: {Fecha}\nDescripcion: {Descripcion}\nCosto: ${Costo}");
+        }
+    }
+}
